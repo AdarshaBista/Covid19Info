155702f03001cb084525c8b38ea1ead0f0d3f4f8
AdarshaBista <adrsh07@gmail.com>
2020-03-28 19:46:42 +0545

    Added api service

---
+++ lib/core/services/api_service.dart
@@ -0,0 +1,103 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import 'package:covid19_info/core/models/app_error.dart';
+import 'package:covid19_info/core/models/faq.dart';
+import 'package:covid19_info/core/models/myth.dart';
+import 'package:covid19_info/core/models/news.dart';
+import 'package:covid19_info/core/models/hospital.dart';
+import 'package:covid19_info/core/models/infected_nepali.dart';
+import 'package:covid19_info/core/models/nepal_infection_data.dart';
+
+class ApiService {
+  static const String NEPALI_CORONA_BASE =
+      'https://nepalcorona.info/api/v1/data/';
+
+  Future<NepalInfectionData> fetchNepalInfectionData() async {
+    try {
+      http.Response res = await http.get(NEPALI_CORONA_BASE + 'nepal');
+      return NepalInfectionData.fromJson(res.body);
+    } catch (e) {
+      throw AppError(
+        message: "Couldn't load nepal infection data!",
+        error: e.toString(),
+      );
+    }
+  }
+
+  Future<List<InfectedNepali>> fetchInfectedNepalese(int start) async {
+    try {
+      http.Response res =
+          await http.get(NEPALI_CORONA_BASE + 'allnepali?start=$start');
+      final Map<String, dynamic> resMap = jsonDecode(res.body);
+      final List<Map<String, dynamic>> infectedList = resMap['data'];
+      return infectedList.map((i) => InfectedNepali.fromMap(i));
+    } catch (e) {
+      throw AppError(
+        message: "Couldn't load FAQ!",
+        error: e.toString(),
+      );
+    }
+  }
+
+  Future<List<News>> fetchNews(int start) async {
+    try {
+      http.Response res =
+          await http.get(NEPALI_CORONA_BASE + 'news?start=$start');
+      final Map<String, dynamic> resMap = jsonDecode(res.body);
+      final List<Map<String, dynamic>> newsList = resMap['data'];
+      return newsList.map((n) => News.fromMap(n));
+    } catch (e) {
+      throw AppError(
+        message: "Couldn't load news!",
+        error: e.toString(),
+      );
+    }
+  }
+
+  Future<List<Myth>> fetchMyths(int start) async {
+    try {
+      http.Response res =
+          await http.get(NEPALI_CORONA_BASE + 'myths?start=$start');
+      final Map<String, dynamic> resMap = jsonDecode(res.body);
+      final List<Map<String, dynamic>> mythList = resMap['data'];
+      return mythList.map((m) => Myth.fromMap(m));
+    } catch (e) {
+      throw AppError(
+        message: "Couldn't load myths!",
+        error: e.toString(),
+      );
+    }
+  }
+
+  Future<List<Faq>> fetchFaqs(int start) async {
+    try {
+      http.Response res =
+          await http.get(NEPALI_CORONA_BASE + 'faqs?start=$start');
+      final Map<String, dynamic> resMap = jsonDecode(res.body);
+      final List<Map<String, dynamic>> faqList = resMap['data'];
+      return faqList.map((f) => Faq.fromMap(f));
+    } catch (e) {
+      throw AppError(
+        message: "Couldn't load FAQ!",
+        error: e.toString(),
+      );
+    }
+  }
+
+  Future<List<Hospital>> fetchHospitals(int start) async {
+    try {
+      http.Response res =
+          await http.get(NEPALI_CORONA_BASE + 'hospitals?start=$start');
+      final Map<String, dynamic> resMap = jsonDecode(res.body);
+      final List<Map<String, dynamic>> hospitalList = resMap['data'];
+      return hospitalList.map((h) => Hospital.fromMap(h));
+    } catch (e) {
+      throw AppError(
+        message: "Couldn't load FAQ!",
+        error: e.toString(),
+      );
+    }
+  }
+}
